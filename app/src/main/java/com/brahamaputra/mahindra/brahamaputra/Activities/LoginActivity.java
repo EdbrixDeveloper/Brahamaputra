005c380909eb175ae13c5953fe529b495df893c1
Shashank <shashank.ko@edbrix.com>
2018-10-29 12:06:39 +0530

    - Login Activity : 1. UI : IME Action set to edit box. 2. Activity : IME Action validation code added.
    By Raj K

--- app/src/main/java/com/brahamaputra/mahindra/brahamaputra/Activities/LoginActivity.java
+++ app/src/main/java/com/brahamaputra/mahindra/brahamaputra/Activities/LoginActivity.java
@@ -5,12 +5,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.brahamaputra.mahindra.brahamaputra.R;
 
@@ -26,35 +29,55 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         assignViews();
-        setListner();
+        setListener();
+        hideKeyboard();
     }
-    private void setListner(){
-        mLoginButtonLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this,DashboardActivity.class));
-            }
-        });
-    }
-
-
 
     private void assignViews() {
         mImageView = (ImageView) findViewById(R.id.imageView);
         mLoginEditTextUsername = (EditText) findViewById(R.id.login_editText_username);
         mLoginEditTextPassword = (EditText) findViewById(R.id.login_editText_password);
+        mLoginEditTextPassword.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    // hide virtual keyboard
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(mLoginEditTextPassword.getWindowToken(), 0);
+                    //doLogin
+                    doLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
         mLoginButtonLogin = (Button) findViewById(R.id.login_button_login);
         getWindow().setSoftInputMode(
                 WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN
         );
     }
 
-    private void closeKeyboard() {
-        View view = this.getCurrentFocus();
-        if (view != null) {
-            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-        }
+    private void setListener(){
+        mLoginButtonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doLogin();
+            }
+        });
+    }
+
+    private void hideKeyboard() {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(mLoginEditTextUsername.getWindowToken(), 0);
+    }
+
+    private void checkValidations(){
+
+    }
+
+    private void doLogin(){
+        startActivity(new Intent(LoginActivity.this,DashboardActivity.class));
     }
 
 
