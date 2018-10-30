cb0e58abe2584b715c1cbd107a7c68be4ce4107a
Vinayak <vinayakp@edbrix.com>
2018-10-30 20:13:33 +0530

    User Profile changes,layout changes done by vinayak

--- app/src/main/java/com/brahamaputra/mahindra/brahamaputra/Activities/UserProfileActivity.java
+++ app/src/main/java/com/brahamaputra/mahindra/brahamaputra/Activities/UserProfileActivity.java
@@ -2,6 +2,8 @@ package com.brahamaputra.mahindra.brahamaputra.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.brahamaputra.mahindra.brahamaputra.R;
@@ -15,10 +17,22 @@ public class UserProfileActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         this.setTitle("Profile");
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.profile_menu, menu);
+        return true;
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
+                finish();
+                return true;
+
+            case R.id.menuLogout:
                 finish();
                 return true;
 
