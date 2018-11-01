7244853f2d3444121f425633bdef74147184958e
Shashank <shashank.ko@edbrix.com>
2018-11-01 15:31:49 +0530

    - Offlinestoragewrapper class added - LandDetailsActivity - added logic to save json while offline
    - Commmit by Raj on 1st Nov 2018

---
+++ app/src/main/java/com/brahamaputra/mahindra/brahamaputra/Data/HotoTransactionData.java
@@ -0,0 +1,25 @@
+package com.brahamaputra.mahindra.brahamaputra.Data;
+
+public class HotoTransactionData {
+
+    private String ticketNo;
+
+    private LandDetailsData landDetailsData;
+
+
+    public String getTicketNo() {
+        return ticketNo;
+    }
+
+    public void setTicketNo(String ticketNo) {
+        this.ticketNo = ticketNo;
+    }
+
+    public LandDetailsData getLandDetailsData() {
+        return landDetailsData;
+    }
+
+    public void setLandDetailsData(LandDetailsData landDetailsData) {
+        this.landDetailsData = landDetailsData;
+    }
+}
