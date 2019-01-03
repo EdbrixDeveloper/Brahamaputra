2c0b145c96fff19c35cbb215fe47e34803a56e2e
shashank <shashank.khochikar@gmail.com>
2019-01-03 19:21:16 +0530

    Commit PM AC Technician activity, PM Site Eb Meter Box by arjun on 03012019.

---
+++ app/src/main/java/com/brahamaputra/mahindra/brahamaputra/Activities/PreventiveMaintenanceSiteEbMeterBoxActivity.java
@@ -0,0 +1,17 @@
+package com.brahamaputra.mahindra.brahamaputra.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.brahamaputra.mahindra.brahamaputra.R;
+import com.brahamaputra.mahindra.brahamaputra.baseclass.BaseActivity;
+
+public class PreventiveMaintenanceSiteEbMeterBoxActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preventive_maintenance_site_eb_meter_box);
+        setTitle("EB METER BOX");
+    }
+}
