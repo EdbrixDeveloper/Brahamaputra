b56cb83af6640191118a4c4e78a7aa5204e736d0
Vinayak <VinayakP@edbrix.com>
2019-01-03 12:41:25 +0530

    Commit Design OF AC Preventive Maintenance By Pranav 03012019

---
+++ app/src/main/java/com/brahamaputra/mahindra/brahamaputra/Activities/PreventiveMaintanceAcFieldEngineerActivity.java
@@ -0,0 +1,15 @@
+package com.brahamaputra.mahindra.brahamaputra.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.brahamaputra.mahindra.brahamaputra.R;
+
+public class PreventiveMaintanceAcFieldEngineerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preventive_maintance_ac_field_engineer);
+    }
+}
