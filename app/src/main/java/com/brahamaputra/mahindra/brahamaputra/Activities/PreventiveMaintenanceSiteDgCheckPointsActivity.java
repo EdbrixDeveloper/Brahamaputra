446b77905869eb3d053476be667a13fa480faa24
shashank <shashank.khochikar@gmail.com>
2019-01-04 11:46:07 +0530

    Commit PM Site- DG Check Points by arjun on 04012019.

---
+++ app/src/main/java/com/brahamaputra/mahindra/brahamaputra/Activities/PreventiveMaintenanceSiteDgCheckPointsActivity.java
@@ -0,0 +1,16 @@
+package com.brahamaputra.mahindra.brahamaputra.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.brahamaputra.mahindra.brahamaputra.R;
+import com.brahamaputra.mahindra.brahamaputra.baseclass.BaseActivity;
+
+public class PreventiveMaintenanceSiteDgCheckPointsActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preventive_maintenance_site_dg_check_points);
+    }
+}
