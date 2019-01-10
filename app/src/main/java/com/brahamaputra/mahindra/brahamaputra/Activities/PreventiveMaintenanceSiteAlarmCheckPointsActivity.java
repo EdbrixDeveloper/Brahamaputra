6860413aa37cff29266cd004aba41aa5bdbfec97
shashank <shashank.khochikar@gmail.com>
2019-01-10 16:53:24 +0530

    Commit  Changes- PM Site Alarm Check and gradile-update Zxing library  activity by arjun on 10012019.

--- app/src/main/java/com/brahamaputra/mahindra/brahamaputra/Activities/PreventiveMaintenanceSiteAlarmCheckPointsActivity.java
+++ app/src/main/java/com/brahamaputra/mahindra/brahamaputra/Activities/PreventiveMaintenanceSiteAlarmCheckPointsActivity.java
@@ -1,6 +1,14 @@
 package com.brahamaputra.mahindra.brahamaputra.Activities;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,14 +19,27 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.brahamaputra.mahindra.brahamaputra.BuildConfig;
 import com.brahamaputra.mahindra.brahamaputra.R;
+import com.brahamaputra.mahindra.brahamaputra.Utils.SessionManager;
+import com.brahamaputra.mahindra.brahamaputra.baseclass.BaseActivity;
+import com.brahamaputra.mahindra.brahamaputra.commons.AlertDialogManager;
+import com.brahamaputra.mahindra.brahamaputra.commons.OfflineStorageWrapper;
 import com.brahamaputra.mahindra.brahamaputra.helper.OnSpinnerItemClick;
 import com.brahamaputra.mahindra.brahamaputra.helper.SearchableSpinnerDialog;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 
-public class PreventiveMaintenanceSiteAlarmCheckPointsActivity extends AppCompatActivity {
+public class PreventiveMaintenanceSiteAlarmCheckPointsActivity extends BaseActivity {
+
+    private static final String TAG = PreventiveMaintenanceSiteAlarmCheckPointsActivity.class.getSimpleName();
+
     private TextView mPreventiveMaintenanceSiteAlarmCheckPointsTextViewDetailsOfWrms;
     private ImageView mPreventiveMaintenanceSiteAlarmCheckPointsButtonDetailsOfWrmsQRCodeScan;
     private ImageView mPreventiveMaintenanceSiteAlarmCheckPointsButtonDetailsOfWrmsQRCodeScanView;
@@ -54,6 +75,25 @@ public class PreventiveMaintenanceSiteAlarmCheckPointsActivity extends AppCompat
     String str_pmSiteAcpRegisterFaultVal = "";
     String str_pmSiteAcpTypeOfFaultVal = "";
 
+    public static final int MY_PERMISSIONS_REQUEST_CAMERA = 100;
+    private String base64StringDetailsOfWrmsQRCodeScan = "";
+    private String imageFileUploadPhotoOfSitePremises;
+    private Uri imageFileUriUploadPhotoOfSitePremises = null;
+
+    public static final String ALLOW_KEY = "ALLOWED";
+    public static final String CAMERA_PREF = "camera_pref";
+
+    private AlertDialogManager alertDialogManager;
+
+    private String userId = "";
+    private String ticketId = "";
+    private String ticketName = "";
+
+    /*private HotoTransactionData hotoTransactionData;
+    private LandDetailsData landDetailsData;*/
+    private OfflineStorageWrapper offlineStorageWrapper;
+    private SessionManager sessionManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,7 +101,9 @@ public class PreventiveMaintenanceSiteAlarmCheckPointsActivity extends AppCompat
         this.setTitle("Alarm Check Points");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         assignViews();
+        checkCameraPermission();
         initCombo();
+        setListner();
     }
 
     private void assignViews() {
@@ -271,6 +313,93 @@ public class PreventiveMaintenanceSiteAlarmCheckPointsActivity extends AppCompat
                 });
             }
         });
+    }
+
+    private void setListner() {
+
+        mPreventiveMaintenanceSiteAlarmCheckPointsButtonDetailsOfWrmsQRCodeScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkCameraPermission()) {
+                    DetailsOfWrmsQRCodeScan();
+                }
+            }
+        });
+
+        mButtonClearDetailsOfWrmsQRCodeScanView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                base64StringDetailsOfWrmsQRCodeScan = "";
+                mButtonClearDetailsOfWrmsQRCodeScanView.setVisibility(View.GONE);
+                mPreventiveMaintenanceSiteAlarmCheckPointsButtonDetailsOfWrmsQRCodeScanView.setVisibility(View.GONE);
+                showToast("Cleared");
+            }
+        });
+
+    }
+
+    private void DetailsOfWrmsQRCodeScan() {
+        try {
+            IntentIntegrator integrator = new IntentIntegrator(this);
+            integrator.setPrompt("Scan a barcode or QRcode");
+            integrator.setOrientationLocked(true);
+            integrator.setRequestCode(MY_PERMISSIONS_REQUEST_CAMERA);
+            integrator.initiateScan();
+
+            //        Use this for more customization
+            //        IntentIntegrator integrator = new IntentIntegrator(this);
+            //        integrator.setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES);
+            //        integrator.setPrompt("Scan a barcode");
+            //        integrator.setCameraId(0);  // Use a specific camera of the device
+            //        integrator.setBeepEnabled(false);
+            //        integrator.setBarcodeImageEnabled(true);
+            //        integrator.initiateScan();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean checkCameraPermission() {
+
+        if (ContextCompat.checkSelfPermission(PreventiveMaintenanceSiteAlarmCheckPointsActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(PreventiveMaintenanceSiteAlarmCheckPointsActivity.this, new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
+        } else {
+            return true;
+        }
+
+
+        return false;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CAMERA:
+                IntentResult result = IntentIntegrator.parseActivityResult(resultCode, data);
+                if (result != null) {
+                    mPreventiveMaintenanceSiteAlarmCheckPointsButtonDetailsOfWrmsQRCodeScanView.setVisibility(View.GONE);
+                    mButtonClearDetailsOfWrmsQRCodeScanView.setVisibility(View.GONE);
+                    if (result.getContents() == null) {
+                        base64StringDetailsOfWrmsQRCodeScan = "";
+                        showToast("Cancelled");
+                    } else {
+                        /*Object[] isDuplicateQRcode = isDuplicateQRcode(result.getContents());
+                        boolean flagIsDuplicateQRcode = (boolean) isDuplicateQRcode[1];
+                        if (!flagIsDuplicateQRcode) {*/
+                        base64StringDetailsOfWrmsQRCodeScan = result.getContents();
+                        if (!base64StringDetailsOfWrmsQRCodeScan.isEmpty() && base64StringDetailsOfWrmsQRCodeScan != null) {
+                            mPreventiveMaintenanceSiteAlarmCheckPointsButtonDetailsOfWrmsQRCodeScanView.setVisibility(View.VISIBLE);
+                            mButtonClearDetailsOfWrmsQRCodeScanView.setVisibility(View.VISIBLE);
+                        }
+                        /*} else {
+                            base64StringDetailsOfWrmsQRCodeScan = "";
+                            showToast("This QR Code Already Used in " + isDuplicateQRcode[0] + " Section");
+                        }*/
+                    }
+                }
+                break;
+        }
     }
 
     @Override
