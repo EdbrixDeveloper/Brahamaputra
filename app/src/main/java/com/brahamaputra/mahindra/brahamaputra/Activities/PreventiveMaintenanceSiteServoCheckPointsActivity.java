a86279e8069cfef93af2ec2eccd7e59c95963b8e
shashank <shashank.khochikar@gmail.com>
2019-01-11 10:26:49 +0530

    Commit  QR Code Code in PM Site- PMS AMF Panel Check activity and Servo check activity  by arjun on 11012019.

--- app/src/main/java/com/brahamaputra/mahindra/brahamaputra/Activities/PreventiveMaintenanceSiteServoCheckPointsActivity.java
+++ app/src/main/java/com/brahamaputra/mahindra/brahamaputra/Activities/PreventiveMaintenanceSiteServoCheckPointsActivity.java
@@ -1,6 +1,10 @@
 package com.brahamaputra.mahindra.brahamaputra.Activities;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,14 +15,23 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.brahamaputra.mahindra.brahamaputra.R;
+import com.brahamaputra.mahindra.brahamaputra.Utils.SessionManager;
 import com.brahamaputra.mahindra.brahamaputra.baseclass.BaseActivity;
+import com.brahamaputra.mahindra.brahamaputra.commons.AlertDialogManager;
+import com.brahamaputra.mahindra.brahamaputra.commons.GlobalMethods;
+import com.brahamaputra.mahindra.brahamaputra.commons.OfflineStorageWrapper;
 import com.brahamaputra.mahindra.brahamaputra.helper.OnSpinnerItemClick;
 import com.brahamaputra.mahindra.brahamaputra.helper.SearchableSpinnerDialog;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class PreventiveMaintenanceSiteServoCheckPointsActivity extends BaseActivity {
+
+
+
     private TextView mPreventiveMaintenanceSiteServoCheckPointsTextViewDetailsOfServo;
     private ImageView mPreventiveMaintenanceSiteServoCheckPointsButtonDetailsOfServoQRCodeScan;
     private ImageView mPreventiveMaintenanceSiteServoCheckPointsButtonDetailsOfServoQRCodeScanView;
@@ -40,14 +53,40 @@ public class PreventiveMaintenanceSiteServoCheckPointsActivity extends BaseActiv
     String str_registerFaultVal;
     String str_typeOfFaultVal;
 
+    public static final int MY_PERMISSIONS_REQUEST_CAMERA = 100;
+    private String base64StringDetailsOfServoQRCodeScan = "";
+    //private String imageFileDetailsOfServoQRCodeScan;
+    //private Uri imageFileUriDetailsOfServoQRCodeScan = null;
+
+    public static final String ALLOW_KEY = "ALLOWED";
+    public static final String CAMERA_PREF = "camera_pref";
+
+    private AlertDialogManager alertDialogManager;
+
+    private String userId = "";
+    private String ticketId = "";
+    private String ticketName = "";
+
+    /*private HotoTransactionData hotoTransactionData;
+    private LandDetailsData landDetailsData;*/
+    private OfflineStorageWrapper offlineStorageWrapper;
+    private SessionManager sessionManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preventive_maintenance_site_servo_check_points);
         this.setTitle("Servo Check Points");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        sessionManager = new SessionManager(PreventiveMaintenanceSiteServoCheckPointsActivity.this);
+        ticketId = sessionManager.getSessionUserTicketId();
+        ticketName = GlobalMethods.replaceAllSpecialCharAtUnderscore(sessionManager.getSessionUserTicketName());
+        userId = sessionManager.getSessionUserId();
+        offlineStorageWrapper = OfflineStorageWrapper.getInstance(PreventiveMaintenanceSiteServoCheckPointsActivity.this, userId, ticketName);
         assignViews();
+        checkCameraPermission();
         initCombo();
+        setListner();
     }
 
     private void assignViews() {
@@ -171,6 +210,93 @@ public class PreventiveMaintenanceSiteServoCheckPointsActivity extends BaseActiv
             }
         });
 
+    }
+
+    private void setListner() {
+
+        mPreventiveMaintenanceSiteServoCheckPointsButtonDetailsOfServoQRCodeScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkCameraPermission()) {
+                    DetailsOfWrmsQRCodeScan();
+                }
+            }
+        });
+
+        mButtonClearDetailsOfServoQRCodeScanView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                base64StringDetailsOfServoQRCodeScan = "";
+                mButtonClearDetailsOfServoQRCodeScanView.setVisibility(View.GONE);
+                mPreventiveMaintenanceSiteServoCheckPointsButtonDetailsOfServoQRCodeScanView.setVisibility(View.GONE);
+                showToast("Cleared");
+            }
+        });
+
+    }
+
+    private void DetailsOfWrmsQRCodeScan() {
+        try {
+            IntentIntegrator integrator = new IntentIntegrator(this);
+            integrator.setPrompt("Scan a barcode or QRcode");
+            integrator.setOrientationLocked(true);
+            integrator.setRequestCode(MY_PERMISSIONS_REQUEST_CAMERA);
+            integrator.initiateScan();
+
+            //        Use this for more customization
+            //        IntentIntegrator integrator = new IntentIntegrator(this);
+            //        integrator.setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES);
+            //        integrator.setPrompt("Scan a barcode");
+            //        integrator.setCameraId(0);  // Use a specific camera of the device
+            //        integrator.setBeepEnabled(false);
+            //        integrator.setBarcodeImageEnabled(true);
+            //        integrator.initiateScan();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean checkCameraPermission() {
+
+        if (ContextCompat.checkSelfPermission(PreventiveMaintenanceSiteServoCheckPointsActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(PreventiveMaintenanceSiteServoCheckPointsActivity.this, new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
+        } else {
+            return true;
+        }
+
+
+        return false;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CAMERA:
+                IntentResult result = IntentIntegrator.parseActivityResult(resultCode, data);
+                if (result != null) {
+                    mPreventiveMaintenanceSiteServoCheckPointsButtonDetailsOfServoQRCodeScanView.setVisibility(View.GONE);
+                    mButtonClearDetailsOfServoQRCodeScanView.setVisibility(View.GONE);
+                    if (result.getContents() == null) {
+                        base64StringDetailsOfServoQRCodeScan = "";
+                        showToast("Cancelled");
+                    } else {
+                        /*Object[] isDuplicateQRcode = isDuplicateQRcode(result.getContents());
+                        boolean flagIsDuplicateQRcode = (boolean) isDuplicateQRcode[1];
+                        if (!flagIsDuplicateQRcode) {*/
+                        base64StringDetailsOfServoQRCodeScan = result.getContents();
+                        if (!base64StringDetailsOfServoQRCodeScan.isEmpty() && base64StringDetailsOfServoQRCodeScan != null) {
+                            mPreventiveMaintenanceSiteServoCheckPointsButtonDetailsOfServoQRCodeScanView.setVisibility(View.VISIBLE);
+                            mButtonClearDetailsOfServoQRCodeScanView.setVisibility(View.VISIBLE);
+                        }
+                        /*} else {
+                            base64StringDetailsOfServoQRCodeScan = "";
+                            showToast("This QR Code Already Used in " + isDuplicateQRcode[0] + " Section");
+                        }*/
+                    }
+                }
+                break;
+        }
     }
 
     @Override
